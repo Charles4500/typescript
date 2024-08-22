78ed737d239fed29d5726a0084bd462e9c38edcf
CharlesKibet <charlesbiegon973@gmail.com>
2024-08-22 17:17:49 +0300

    Stylinf

--- typescript-tutorial/src/components/TodoList.tsx
+++ typescript-tutorial/src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Todo } from '../model'
 import './styles.css'
+import SingleTodo from './SingleTodo'
 interface Props{
   todos:Todo[]
   setTodos:React.Dispatch<React.SetStateAction<Todo[]>>
@@ -8,7 +9,7 @@ interface Props{
 const TodoList:React.FC<Props>= ({todos,setTodos}) => {
   return (
     <div className='todos'>{todos.map(todo=>(
-      <li>{todo.todo}</li>
+      <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos}/>
     ))}</div>
   )
 }
